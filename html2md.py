6ca249a3d2d8ee51211155c8c5c5325819becf86
ilovlowphy <ilovlowphy@gmail.com>
2021-04-27 08:08:07 +0800

    create the html2md

--- html2md.py
+++ html2md.py
@@ -1,11 +1,16 @@
 import os
+import pypandoc
 
 def main():
     for root, dirs, files in os.walk(".", topdown=False):
         for name in files:
-            print(os.path.join(root, name))
-        for name in dirs:
-            print(os.path.join(root, name))
+            input_file = os.path.join(root, name)
+            file_name, ext = os.path.splitext(input_file)
+            if ext==".html":
+                output_file = file_name+".md"
+                pypandoc.convert(input_file, 'md', format='html',outputfile=output_file)
+        # for name in dirs:
+        #     print(os.path.join(root, name))
 
 if __name__=="__main__" :
     main()
